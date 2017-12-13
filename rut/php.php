e9bb4ed3a7b260c2286724270821c1131fd41ef9
Alexis Abarca <99387+alexisabarca@users.noreply.github.com>
2017-12-13 16:58:15 -0300

    Update and rename php.php.html to php.php

--- rut/php.php
+++ rut/php.php
@@ -4,14 +4,14 @@
 
 <p>Enviado por Luis Dujovne</p>
 <pre>
-&lt;?php
+<?php
 function dv($r){$s=1;for($m=0;$r!=0;$r/=10)$s=($s+$r%10*(9-$m++%6))%11;
 return chr($s?$s+47:75);}
-?&gt;
+?>
 
 </pre>
 
-<form action="http://users.dcc.uchile.cl/~mortega/microcodigos/validarrut/php.php#" method="post">
+<form action="" method="post">
 <input type="text" size="10" maxlength="10" name="rut" value="">
 &nbsp; - &nbsp;
 <input type="text" size="1" maxlength="1" name="dv" value="">
